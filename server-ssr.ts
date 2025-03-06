c10334354fe69cc0cb97118cd03ed38f60c43c71
Geoo Chi <geoochi@geoochi.com>
2025-03-06 21:13:12 +0800

    v1.0

--- server-ssr.ts
+++ server-ssr.ts
@@ -122,7 +122,7 @@ app.use('*', async (req, res, next) => {
   }
 })
 
-const PORT = 5000
+const PORT = 3004
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
